cc8ea6fbb0c35bba3f9dc2d304bc523fa5f46db4
Qiong Yu <sb15897@student.samk.fi>
2024-11-22 12:45:20 +0200

    update main.py

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import addition
+import subtraction
+import multiplication
+import squareroot
 
 print("Using addition")
-x = add_two_numbers(1, 2)
-print("1 + 2 = " + x)
+x = addition.add_two_numbers(1, 2)
+print("1 + 2 = " + str(x))
+
+print("Using subtraction")
+x = subtraction.subtract_two_numbers(1, 2)
+print("1 - 2 = " + str(x))
+
+print("Using multiply_two_numbers")
+x = multiplication.multiply_two_numbers(1, 2)
+print("1 * 2 = " + str(x))
+
+print("Using calculate_square_root")
+x = squareroot.calculate_square_root(2)
+print("square root of 2 = " + str(x))
